491f0cd504077d1cfe3c1b8d2d3c3f94128d5db9
Neha Narula <narula@gmail.com>
2016-09-23 16:28:09 -0400

    run sim

---
+++ run.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+CPUPROFILE = "~/data/profiles/b.prof"
+
+pushd /scratch/neha/bitcoindir &&
+     if [ "$(pwd)" == '/scratch/neha/bitcoindir' ];
+     then
+         git reset --hard && git clean -f -d
+     fi
+popd
+
+#LD_PRELOAD="/usr/lib/libprofiler.so" CPUPROFILE=$(CPUPROFILE) src/bitcoind -simulation -datadir=/scratch/neha/bitcoindir -simdatadir=/scratch/simdata -start=20160630 -end=20160701 -loadmempool=0 -disablewallet -debug=bench
